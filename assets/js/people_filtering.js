8502156204a2dda6f9539b36c885edffb97b8375
sof202 <s.o.fletcher@exeter.ac.uk>
2024-06-21 12:00:06 +0100

    add a filtering script for person profiles
    The script should filter through everyone one by one

---
+++ assets/js/people_filtering.js
@@ -0,0 +1,42 @@
+// Get all the filter buttons
+const allButton = document.getElementById('All')
+const graButton = document.getElementById('GRA');
+const piButton = document.getElementById('PI');
+const phdButton = document.getElementById('PhD');
+const postdocButton = document.getElementById('Postdoc');
+
+// Get all the profiles
+const profiles = document.querySelectorAll('div.person-profile');
+
+allButton.addEventListener('click', () => filterProfiles('all'));
+graButton.addEventListener('click', () => filterProfiles('GRA'));
+piButton.addEventListener('click', () => filterProfiles('PI'));
+phdButton.addEventListener('click', () => filterProfiles('PhD'));
+postdocButton.addEventListener('click', () => filterProfiles('PostDoc'));
+
+// Show all profiles with fade-in effect initially
+profiles.forEach((profile,index) => {
+    profile.classList.add('fade-in');
+    setTimeout(() => {
+    profile.classList.add('show');
+    }, 200 * (index + 1))
+});
+
+// Function to filter profiles based on the link clicked with fade-in effect
+function filterProfiles(category) {
+    profiles.forEach(profile => {
+        profile.classList.remove('show');
+        profile.style.display = 'none';
+    })
+    profiles.forEach((profile, index) => {
+        if (category === 'all' || profile.classList.contains(category)) {
+            setTimeout(() => {
+                profile.style.display = 'flex';
+                setTimeout(() => {
+                    profile.classList.add('show');
+                }, 50); // Adjust this delay as needed for the fade-in effect
+            }, 200 * (index + 1)); // Initial delay for each profile
+        }
+    });
+}
+
