bd4985d607896c2bfd8ba8261865f4b52d990726
sof202 <s.o.fletcher@exeter.ac.uk>
2024-08-15 14:13:30 +0100

    feat: add script to find bibtex files for user

---
+++ scripts/update-bibliography.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPTS_DIR="$(realpath "$(dirname "$0")")"
+PROCESSED_PUBLICATION_DATA="$SCRIPTS_DIR/../assets/data/publications.json"
+RAW_PUBLICATION_DIR=$(realpath "$SCRIPTS_DIR/../raw_bibliography")
+PYTHON_DIR="$SCRIPTS_DIR/processing_scripts/"
+
+raw_publication_data=$(find "${RAW_PUBLICATION_DIR}" -name "*.bib")
+
+if [[ -z "${raw_publication_data}" ]]; then
+    echo "ERROR No .bib files found in ${RAW_PUBLICATION_DIR}."
+fi
+
+cat << EOF
+Found these raw bibliographies: 
+${raw_publication_data}
+
+Please copy and paste the one you want to use with the publications page
+then hit enter.
+
+EOF
+
+read -r bibliography_path
+
+if [[ -f "${bibliography_path}" ]]; then
+    python "${PYTHON_DIR}/bibtex_to_json.py" \
+        "${bibliography_path}" \
+        "${PROCESSED_PUBLICATION_DATA}"
+else
+echo "ERROR: ${bibliography_path} does not exist. Make sure you typed it correctly"
+fi
+
+
