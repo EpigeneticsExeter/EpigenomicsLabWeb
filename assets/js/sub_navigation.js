57693c4f1807f5cfacbb617f853eecf10a752756
sof202 <s.o.fletcher@exeter.ac.uk>
2024-08-16 12:08:13 +0100

    feat: overhaul of header
    The nav bar now works by expanding down into the header, instead of into
    the body. This also has had a nice side benefit of making the logo
    bigger and therefore a little easier to see what it is.
    
    It is a little more complex however, to get the menu to work flawlessly
    (hopefully) it requires some javascript (to ensure the visibility works
    as intended).

---
+++ assets/js/sub_navigation.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navItems = document.querySelectorAll('.nav-item');
+    const allSubNavs = document.querySelectorAll('.sub-nav');
+    const mainNav = document.querySelector('.main-nav');
+
+    navItems.forEach(item => {
+        const link = item.querySelector('.nav-item-link');
+        const subNav = item.querySelector('.sub-nav');
+
+
+        link.addEventListener('mouseenter', function() {
+            allSubNavs.forEach(item => {
+                item.style.opacity = '0';
+                item.style.visibility = 'hidden';
+                item.style.transition = "opacity 0s, visibility 0s"
+            });
+            if (subNav) {
+                subNav.style.opacity = '1'; 
+                subNav.style.visibility = 'visible'; 
+                subNav.style.transition = "opacity 0.5s, visibility 1s"
+            }
+        });
+
+        link.addEventListener('mouseleave', function() {
+            setTimeout(() => {
+                if (!subNav.style.opacity == 1) {
+                    subNav.style.opacity = '0'; 
+                    subNav.style.visibility = 'hidden'; 
+                    subNav.style.transition = "opacity 0.5s, visibility 1s"
+                }
+            }, 100);
+        });
+
+    });
+
+    mainNav.addEventListener('mouseleave', function() {
+        allSubNavs.forEach(item => {
+            item.style.opacity = '0';
+            item.style.visibility = 'hidden';
+            item.style.transition = "opacity 0.5s, visibility 1s"
+        });
+    });
+
+});
