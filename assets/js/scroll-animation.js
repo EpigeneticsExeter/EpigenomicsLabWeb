62542214786860cc82d15491e17d0e52a436983a
sof202 <s.o.fletcher@exeter.ac.uk>
2024-09-24 10:10:17 +0100

    feat: add scroll animation to home page

---
+++ assets/js/scroll-animation.js
@@ -0,0 +1,24 @@
+const subTeamPhotos = document.querySelectorAll(".sub-team-photo");
+const subTeamInfo = document.querySelectorAll(".sub-team-info");
+
+const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5
+};
+
+const observer = new IntersectionObserver(function(elements, observer) {
+    elements.forEach(element => {
+        if (element.isIntersecting) {
+            element.target.classList.add('show')
+        }
+    });
+}, observerOptions);
+
+subTeamPhotos.forEach(photo => {
+    observer.observe(photo)
+});
+
+subTeamInfo.forEach(info => {
+    observer.observe(info)
+});
