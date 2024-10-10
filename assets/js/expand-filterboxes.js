860fb5bd83f572cb742a74491e66671da261aa29
sof202 <s.o.fletcher@exeter.ac.uk>
2024-10-10 10:59:40 +0100

    fix: update filter box behaviour
    Filter box opens when you click anywhere in the box. The box then closes
    upon clicking on any button, the 'filter' text or pressing enter in the
    search box.

--- assets/js/expand-filterboxes.js
+++ assets/js/expand-filterboxes.js
@@ -1,6 +1,7 @@
 mobileModeWidth = 1000;
 
 if (globalThis.innerWidth < mobileModeWidth) {
+    const filterContainer = document.querySelector(".filter-container");
     const filterText = document.getElementById("filter-text");
     const filterGrid = document.querySelector(".filter-grid");
     const searchBox = document.querySelector(".search-box");
@@ -16,35 +17,41 @@ if (globalThis.innerWidth < mobileModeWidth) {
     };
 
     const showElements = () => {
+        filterText.style.color = "grey";
         filterGrid.style.display = "grid";
         if (searchBox) {
             searchBox.style.display = "flex";
-        }
-    };
-
-    hideElements();
-
-    filterText.onclick = function () {
-        filterText.style.color = "grey";
-        if (filterGrid.style.display === "none") {
-            showElements();
-        } else {
-            hideElements();
         }
         setTimeout(() => {
             filterText.style.color = "white";
         }, 150);
     };
 
+    hideElements();
+
+    filterContainer.onclick = function () {
+        if (filterGrid.style.display === "none") {
+            showElements();
+        }
+    };
+    filterText.onclick = function (event) {
+        if (filterGrid.style.display === "grid") {
+            event.stopPropagation();
+            hideElements();
+        }
+    };
+
     buttons.forEach((button) => {
-        button.onclick = hideElements;
+        button.onclick = function (event) {
+            event.stopPropagation();
+            hideElements();
+        };
     });
 
     if (searchBox) {
         const searchInput = document.getElementById("searchInput");
         searchInput.addEventListener("keydown", function (event) {
             if (event.key === "Enter") {
-                console.log("happened");
                 hideElements();
             }
         });
